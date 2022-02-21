7faa080dc918b286ce3955a6c1280857da7a6b5e
kivoksamir <upahmadalkordee@gmail.com>
2022-02-21 15:11:27 +0100

    the_type & name fun

--- way-better.php
+++ way-better.php
@@ -1,23 +1,23 @@
 <?php
-$text= "Creating new order...|
-A calzone pizza should be sent to koen.|
-The address: a yacht in Antwerp.
-The bill is €10.|
-Order finished.|
 
-Creating new order...|
-A marguerita pizza should be sent to manuele.|
-The address: somewhere in Belgium.|
-The bill is €5.|
-Order finished.|
+    function order($type, $name) 
+    {
+        if($name == 'koen'){
+                $address = 'a yacht in Antwerp';
+            } elseif($name == 'manuele'){
+                $address = 'somewhere in Belgium';
+            } elseif ($name == 'students') {
+            $address = 'BeCode office';
+            }
+        
+        echo $Message = 'Creating new order... <br/> A ' 
+        . $type .  ' pizza should be sent to ' 
+        . $name . ". <br/>
+        The address: {$address}. 
+        <br/>The bill is $price.<br/> 
+        Order Finished <br>";
+    }
 
-Creating new order...|
-A golden pizza should be sent to students.|
-The address: BeCode office.|
-The bill is €100.|
-Order finished";
+    
 
-$info_array= explode("|",$text);
-echo "<pre>";
-print_r($info_array);
-echo "</pre>";
+   
